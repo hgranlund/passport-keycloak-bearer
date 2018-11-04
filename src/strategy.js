003bfb4d81a65027d833a7d770a0138a1a0047b8
Simen Haugerud Granlund <simen@hgranlund.com>
2018-11-04 12:49:49 +0100

    Added docker-compse to run å local keycloack instance

--- src/strategy.js
+++ src/strategy.js
@@ -6,21 +6,21 @@ const log = getLogger('Keycloak: Bearer strategy')
 
 const verifyOptions = (options) => {
   if (!options || typeof options !== 'object')
-  throw new Error('KeycloakBearerStrategy: options is required');
+    throw new Error('KeycloakBearerStrategy: options is required');
   if (!options.realm)
-  throw new Error('KeycloakBearerStrategy: realm is required');
+    throw new Error('KeycloakBearerStrategy: realm is required');
   if (!options.clientId || options.clientID === '')
-  throw new Error('KeycloakBearerStrategy: clientId cannot be empty');
+    throw new Error('KeycloakBearerStrategy: clientId cannot be empty');
   if (!options.host || options.host === '')
-  throw new Error('KeycloakBearerStrategy: host cannot be empty');
-  if(options.customLogger){
-    if(typeof options.customLogger.error !== 'function')
+    throw new Error('KeycloakBearerStrategy: host cannot be empty');
+  if (options.customLogger) {
+    if (typeof options.customLogger.error !== 'function')
       throw new Error('KeycloakBearerStrategy: customLogger must have a error function');
-    if(typeof options.customLogger.warn !== 'function')
+    if (typeof options.customLogger.warn !== 'function')
       throw new Error('KeycloakBearerStrategy: customLogger must have a warn function');
-    if(typeof options.customLogger.info !== 'function')
+    if (typeof options.customLogger.info !== 'function')
       throw new Error('KeycloakBearerStrategy: customLogger must have a info function');
-    if(typeof options.customLogger.debug !== 'function')
+    if (typeof options.customLogger.debug !== 'function')
       throw new Error('KeycloakBearerStrategy: customLogger must have a debug function');
   }
 }
@@ -29,10 +29,10 @@ export default class KeycloakBearerStrategy extends Strategy {
   constructor(options, verify) {
     super();
     verifyOptions(options);
-    
+
     this.log = log;
     this.log.levels('console', options.loggingLevel || 'warn');
-    this.log = options.customLogger? options.customLogger: log;
+    this.log = options.customLogger ? options.customLogger : log;
 
     this.userVerify = verify || this.success;
     this.options = options;
@@ -106,21 +106,21 @@ export default class KeycloakBearerStrategy extends Strategy {
 
   authenticate(req) {
     const token = this.tokenFromReq(req);
-    if (token) {
-      this.verifyOnline(token)
-        .then((verifiedToken) => {
-          if (!verifiedToken) {
-            this.failWithLog('Unable to verify token');
-          } else {
-            this.handleVerifiedToken(req, verifiedToken);
-          }
-        }).catch((error) => {
-          if (error.response) {
-            this.failWithLog(`Auth server gave us a  "${error.message}"`);
-          } else {
-            this.failWithLog(error);
-          }
-        });
-    }
+    if (!token) return;
+
+    this.verifyOnline(token)
+      .then((verifiedToken) => {
+        if (!verifiedToken) {
+          this.failWithLog('Unable to verify token');
+        } else {
+          this.handleVerifiedToken(req, verifiedToken);
+        }
+      }).catch((error) => {
+        if (error.response) {
+          this.failWithLog(`Auth server gave us a  "${error.message}"`);
+        } else {
+          this.failWithLog(error);
+        }
+      });
   }
 }

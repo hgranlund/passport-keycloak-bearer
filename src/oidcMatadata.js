2090db4d10052fa4992ae66041b38f374961e42d
Simen Granlund <simen@hgranlund.com>
2018-11-23 09:51:45 +0100

    Handling rejected promise

--- src/oidcMatadata.js
+++ src/oidcMatadata.js
@@ -13,7 +13,7 @@ export default class OIDCMatadata {
 
   getPemKeys() {
     return new Promise((resolve, reject) => {
-      if (this.keys) {
+      if (Array.isArray(this.keys) && this.keys.length > 0) {
         return resolve(this.keys);
       }
       return request.get(this.jwksUrl, { json: true }, (err, response, body) => {
@@ -21,11 +21,9 @@ export default class OIDCMatadata {
           return reject(err);
         }
         if (response.statusCode !== 200) {
-          return reject(
-            new Error(`Error: ${response.statusCode} Cannot get AAD signing Keys`),
-          );
+          return reject(new Error(`Error: ${response.statusCode} Cannot get AAD signing Keys`));
         }
-        if (!body.keys || body.keys.lenght === 0) {
+        if (!Array.isArray(body.keys)) {
           return reject(new Error('We got no AAD signing Keys'));
         }
         this.keys = body.keys.map(key => ({ ...key, pemKey: rsaPublicKeyPem(key.n, key.e) }));
@@ -42,16 +40,21 @@ export default class OIDCMatadata {
         this.log.info('The access token has expired');
       }
       this.log.debug(`Got token with kid: ${token.header.kid}`);
-      return this.getPemKeys().then((keys) => {
-        const keyforToken = keys.find(key => key.kid === token.header.kid);
-        if (!keyforToken) {
-          const err = new Error(`No key matching kid ${token.header.kid}`);
+      return this.getPemKeys()
+        .then((keys) => {
+          const keyforToken = (keys || []).find(key => key.kid === token.header.kid);
+          if (!keyforToken) {
+            const err = new Error(`No key matching kid ${token.header.kid}`);
+            this.log.warn(err.message);
+            done(err);
+          } else {
+            done(null, keyforToken.pemKey);
+          }
+        })
+        .catch((err) => {
           this.log.warn(err.message);
           done(err);
-        } else {
-          done(null, keyforToken.pemKey);
-        }
-      });
+        });
     } catch (error) {
       this.log.warn(error.message);
       return done(error);

c8f4d1b75a2a3ffb55ef442966051aec2b7338f3
Dimava <dimava2@ya.ru>
2023-06-14 17:11:59 +0300

    add JSDoc

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,11 +6,44 @@ class KeycloakBearerStrategy extends Strategy {
   constructor(options: KeycloakBearerStrategy.Options, verify?: VerifyCallback)
 }
 namespace KeycloakBearerStrategy{
-  interface Options extends Omit<StrategyOptions, 'jwtFromRequest' | 'secretOrKey' | 'secretOrKeyProvider'> {
-	realm: string
-	url: string
-	customLogger?: Record<'error' | 'warn' | 'info' | 'debug', (...a: any) => any>
-	jwtFromRequest?: StrategyOptions['jwtFromRequest']
+  interface Options {
+  /** Keycloak auth url. For instance: https://keycloak.dev.org/auth. */
+  url: string
+  /** Your realm. */
+  realm?: string
+  /**
+   * Whether you want to use req as the first parameter in the verify callback.
+   *
+   * See section 5.1.1.3 for more details.
+   * @default false
+   */
+  passReqToCallback?: StrategyOptions['passReqToCallback']
+  /**
+   * Logging level
+   * @default 'warn
+   */
+  loggingLevel?: 'debug' | 'info' | 'warn' | 'error'
+  /** Custom logging instance. */
+  customLogger?: Record<'debug' | 'info' | 'warn' | 'error', (...a: any[]) => any>
+  /** If defined the token issuer (iss) will be verified against this value. */
+  issuer?: StrategyOptions['issuer']
+  /** If defined, the token audience (aud) will be verified against this value. */
+  audience?: StrategyOptions['audience']
+  /**
+   * List of strings with the names of the allowed algorithms.
+   * @example ["HS256", "HS384"].
+   * @default ["HS256"]
+   */
+  algorithms?: StrategyOptions['algorithms']
+  /** If true do not validate the expiration of the token. */
+  ignoreExpiration?: StrategyOptions['ignoreExpiration']
+  /** This value can be set according passport-jwt if this options is not used, passport-keycloak-bearer will obtain jwt from http header Auth as a Bearer token. */
+  jwtFromRequest?: StrategyOptions['jwtFromRequest']
+  /** passport-keycloak-bearer is verifying the token using jsonwebtoken. Pass here an options object for any other option you can pass the jsonwebtoken verifier. (i.e maxAge) */
+  jsonWebTokenOptions?: StrategyOptions['jsonWebTokenOptions']
+
+  // secretOrKey: removed
+  // secretOrKeyProvider: removed
   }
 }
 
